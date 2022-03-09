3b91aea433bd8a2b096d805a8338808b85da7bc4
Stephan Steiner <stephan.steiner@gmail.com>
2022-03-09 17:10:07 +0100

    addes missing files

---
+++ Sample/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sample.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ValueBefore(this string source, string val, bool returnFullValueWhenValueNotFound = false)
+        {
+            if (val == null)
+                throw new ArgumentException(nameof(val));
+            if (source == null)
+                return source;
+            int index = source.IndexOf(val);
+            if (index > 0)
+                return source[..index];
+            else if (returnFullValueWhenValueNotFound)
+                return source;
+            return string.Empty;
+        }
+
+        public static string ValueAfter(this string source, string val, bool returnFullValueWhenValueNotFound = false)
+        {
+            if (val == null)
+                throw new ArgumentException(nameof(val));
+            if (source == null)
+                return source;
+            int index = source.IndexOf(val);
+            if (index > 0)
+                return source[(index + 1)..];
+            else if (source.StartsWith(val))
+                return source[val.Length..];
+            if (returnFullValueWhenValueNotFound)
+                return source;
+            return string.Empty;
+        }
+    }
+}
---
+++ Sample/SimpleParsedFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class SimpleParsedFilter
+    {
+
+        public List<AttributeFilter> AttributeFilters { get; set; } = new List<AttributeFilter>();
+
+    }
+
+    public class AttributeFilter
+    {
+        public Operator Operator { get; set; }
+
+        public string Attribute { get; set; }
+
+        public string AttributeValue { get; set; }
+    }
+
+    public enum Operator { Equals, Substring, GreaterOrEqual, LessOrEqual }
+}
